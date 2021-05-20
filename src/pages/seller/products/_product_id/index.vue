3c117828908e6b0738bade8c9f84189857a55a46
Vishal Pradhan <vishu_vishal_bi@live.com>
2021-05-20 20:35:09 +0530

    Product unpublished

--- src/pages/seller/products/_product_id/index.vue
+++ src/pages/seller/products/_product_id/index.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
-    <SocialHead
-      :title="productDetails.productTitle"
-      :description="productDetails.productDescription"
-      :image="productDetails.productPhotos[0]"
-    />
-    <h2 class="text-center">{{ productDetails.productTitle }}</h2>
-    <product-details-card />
-    <!-- <v-card outlined class="my-2 rounded-lg">
+    <div v-if="productDetails.published">
+      <SocialHead :title="productDetails.productTitle" :description="productDetails.productDescription"
+        :image="productDetails.productPhotos[0]" />
+      <h2 class="text-center">{{ productDetails.productTitle }}</h2>
+      <product-details-card />
+      <!-- <v-card outlined class="my-2 rounded-lg">
       <v-card-title class="justify-center">Sharing Options</v-card-title>
       <v-card-actions>
         <v-row class="justify-space-around">
@@ -32,15 +30,19 @@
         </v-row>
       </v-card-actions>
     </v-card> -->
-    <shipping-information-form />
-    <seller-special-instructions />
-    <you-pay />
-    <checkout-payment-stripe />
+      <shipping-information-form />
+      <seller-special-instructions />
+      <you-pay />
+      <checkout-payment-stripe />
+    </div>
+    <div v-else class="text-center">
+      The product is no longer available for sale
+    </div>
   </div>
 </template>
 
 <script>
-  import {fireDb, fireStorage, hostServer, analytics} from "~/plugins/firebase.js"
+  import { fireDb, fireStorage, hostServer, analytics } from "~/plugins/firebase.js"
 
   export default {
     middleware: "router-auth",
@@ -54,7 +56,7 @@
 
       var productDetails = {}
       if (product_ref.data()) {
-        productDetails = {...product_ref.data()} //, productId: docId}
+        productDetails = { ...product_ref.data() } //, productId: docId}
         context.store.commit("SET_PRODUCT_DETAILS", productDetails)
         context.store.commit("SET_PRODUCT_ID", product_id)
       } else {
@@ -69,16 +71,16 @@
 
       var seller
       seller_ref.forEach((doc) => {
-        seller = {...doc.data()}
+        seller = { ...doc.data() }
       })
       //This code should be refactored to set seller (will require changing user to seller everywhere)
       context.store.commit("SET_SELLER", seller)
       // console.log(seller)
 
       var url = hostServer + context.route.path
-      return {product_id, productDetails, url}
+      return { product_id, productDetails, url }
     },
-    mounted(){
+    mounted() {
       analytics().logEvent('seller_view', {
         seller_id: this.$store.state.productDetails.seller_id,
         product_id: this.$store.state.product_id
@@ -100,7 +102,7 @@
       return {
         title: "Home page",
         seller_id: "",
-        product_id:""
+        product_id: ""
       }
     },
   }
